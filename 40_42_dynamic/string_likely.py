ae2b17e013d3bd98f35ff706b307d7dc6974fbfd
farmerhoo <farmerhoo@163.com>
2020-02-03 22:38:23 +0800

    Add dynamic planning code for longest substrate string problem

--- 40_42_dynamic/string_likely.py
+++ 40_42_dynamic/string_likely.py
@@ -73,13 +73,94 @@ def levenshteinDP(s1: str, s2: str) -> int:
                 table[i][j] = min(table[i-1][j]+1, table[i][j-1]+1, table[i-1][j-1]+1)
     return table[n1-1][n2-1]
 
+'''
+回溯算法计算最长公共子串
+递归思路：
+    1. 基线条件：任何一个字符串完全考察所有字符
+    2. 递归：
+        如果 a[i]与 b[j]互相匹配，我们将最大公共子串长度加一，并且继续考察 a[i+1]和 b[j+1]
+        如果 a[i]与 b[j]不匹配，最长公共子串长度不变，这个时候，有两个不同的决策路线：
+            删除 a[i]，或者在 b[j]前面加上一个字符 a[i]，然后继续考察 a[i+1]和 b[j]
+            删除 b[j]，或者在 a[i]前面加上一个字符 b[j]，然后继续考察 a[i]和 b[j+1]
+'''
+def substr(s1: str, s2: str) -> int:
+    n1 = len(s1)
+    n2 = len(s2)
+    max_sub = 0
+    def substr_bt(i: int, j: int, lcs: int):
+        '''
+        Args:
+            i(int) - s1考察的字符位置
+            j(int) - s2考察的字符位置
+            lcs(int) - 考察位置时的最长公共子串
+        '''
+        # 递归的基线条件
+        if i == n1 or j == n2:
+            nonlocal max_sub
+            if max_sub < lcs:
+                max_sub = lcs
+            return 
+        # 递归条件
+        if s1[i] == s2[j]:
+            substr_bt(i+1, j+1, lcs+1)
+        else:
+            substr_bt(i, j+1, lcs)
+            substr_bt(i+1, j, lcs)
+    substr_bt(0, 0, 0)
+    return max_sub
+
+'''
+动态规划计算最长公共子串
+递推公式：
+    1. 如果a[i]==b[j]:
+        max_str(i, j) = max(max_str(i-1, j-1)+1, max_str(i-1, j), max(i, j-1))
+    2. 如果a[i]!=b[j]:
+        max_str(i, j) = max(max_str(i-1, j-1), max_str(i-1, j), max(i, j-1))
+'''
+def substr_dp(s1: str, s2: str) -> int:
+    n1 = len(s1)
+    n2 = len(s2)
+    table = [[0] * n2 for i in range(n1)]
+    # 初始化第0行
+    for j in range(n2):
+        if s1[0] == s2[j]:
+            table[0][j] = 1
+        elif j != 0:
+            table[0][j] = table[0][j-1]
+        else:
+            table[0][j] = 0
+    # 初始化第0列
+    for i in range(n1):
+        if s1[i] == s2[0]:
+            table[i][0] = 1
+        elif i != 0:
+            table[i][0] = table[i-1][0]
+        else:
+            table[i][0] = 0
+    # 填表
+    for i in range(1, n1):
+        for j in range(1, n2):
+            if s1[i] == s2[j]:
+                table[i][j] = max(table[i-1][j-1]+1, 
+                                  table[i-1][j], 
+                                  table[i][j-1])
+            else:
+                table[i][j] = max(table[i-1][j-1], 
+                                  table[i-1][j], 
+                                  table[i][j-1])
+    return table[n1-1][n2-1]
 
 if __name__ == '__main__':
-    s1 = 'eeee'
-    s2 = 'eeee'
+    s1 = 'aeee'
+    s2 = 'eaee'
     result = levenshteinBT(s1, s2)
     print(result)
     result1 = levenshteinDP(s1, s2)
     print(result1)
+
+    max_sub_str = substr(s1, s2)
+    print(max_sub_str)
+    max_sub_str1 = substr_dp(s1, s2)
+    print(max_sub_str1)
 
 
